93b891d15267e4c0fe9e6527cd6ad996431a2110
agrebenisan <agrebenisan@tenstorrent.com>
2023-06-19 12:59:02 -0700

    Backup semi-optimized matmul

--- main.cpp
+++ main.cpp
@@ -1,18 +1,58 @@
 #include "nn.hpp"
+#include <chrono>
+
+void time_naive_matmul() {
+    constexpr size_t dim = 200;
+    auto A = create_matrix<dim, dim, float>(0);
+    auto B = create_matrix<dim, dim, float>(0);
+
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000; i++) {
+        auto C = naive_matmul(A, B);
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = (end - start);
+    cout << "Naive matmul time took: " << elapsed_seconds.count() << "s" << endl;
+}
+
+void time_matmul() {
+    constexpr size_t dim = 200;
+    auto A = create_matrix<dim, dim, float>(0);
+    auto B = transpose(create_matrix<dim, dim, float>(0));
+
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 1000; i++) {
+        auto C = matmul(A, B);
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = (end - start);
+    cout << "Matmul time took: " << elapsed_seconds.count() << "s" << endl;
+}
+
 
 int main() {
     // Some data structure to represent matrices
     array<array<int, 2>, 2> matrix = create_matrix<2, 2, int>(0);
 
     // Print out matrices
-    print_matrix(matrix);
+    // print_matrix(matrix);
 
     // Naive matrix multiplication
     auto A = create_matrix<2, 2, int>(0);
     auto B = create_matrix<2, 2, int>(0);
-    auto C = naive_matmul(A, B);
+    // auto C = naive_matmul(A, B);
 
-    print_matrix(C);
+    // print_matrix(C);
+
+    auto D = create_matrix<2, 2, int>(0);
+    auto E_T = transpose(create_matrix<2, 2, int>(0));
+    // print_matrix(E_T);
+    auto F = matmul(D, E_T);
+    // print_matrix(F);
+    // auto F = create_matrix<2, 2, int>(0);
+
+    time_naive_matmul();
+    time_matmul();
 
 
     // Optimizing matrix multiplication
--- nn.tpp
+++ nn.tpp
@@ -35,14 +35,46 @@ void print_matrix(const matrix<nr, nc, T>& matrix) {
 
 template <size_t nr, size_t inner, size_t nc, typename T>
 matrix<nr, nc, T> naive_matmul(const matrix<nr, inner, T>& A, const matrix<inner, nc, T>& B) {
-    matrix<nr, nc, T> result;
+    matrix<nr, nc, T> result = {};
 
     for (size_t c = 0; c < nc; c++) {
         for (size_t r = 0; r < nr; r++) {
-            result[r][c] = 0;
+            T accum = 0;
             for (size_t i = 0; i < inner; i++) {
-                result[r][c] += A[r][i] * B[i][c];
+                accum += A[r][i] * B[i][c];
             }
+            result[r][c] = accum;
+        }
+    }
+
+    return result;
+}
+
+template <size_t nr, size_t nc, typename T>
+matrix<nc, nr, T> transpose(const matrix<nr, nc, T>& A) {
+    matrix<nr, nc, T> A_T;
+    for (size_t r = 0; r < nr; r++) {
+        for (size_t c = 0; c < nc; c++) {
+            A_T[r][c] = A[c][r];
+        }
+    }
+    return A_T;
+}
+
+template <size_t nr, size_t inner, size_t nc, typename T>
+matrix<nr, nc, T> matmul(const matrix<nr, inner, T>& A, const matrix<inner, nc, T>& B) {
+    matrix<nr, nc, T> result = {};
+
+    for (size_t r = 0; r < nr; r++) {
+        const auto& A_row = A[r];
+        auto& res_row     = result[r];
+        for (size_t c = 0; c < nc; c++) {
+            const auto& B_col = B[c];
+            T accum = 0;
+            for (size_t i = 0; i < inner; i++) {
+                accum += A_row[i] * B_col[i];
+            }
+            res_row[c] = accum;
         }
     }
 
